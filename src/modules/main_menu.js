31b00bab3b335ea62f02563f8f88e13fa4250fbc
“Yeiosn <jeison.zk6@hotmail.com>
2018-02-07 15:07:34 -0500

    adding elements to the menu

--- src/modules/main_menu.js
+++ src/modules/main_menu.js
@@ -16,17 +16,28 @@ export class Mainmenu {
   setElementsDom () {
     this.elements.openedmenu = document.querySelector('.container')
     this.elements.header = document.querySelector('.header-global')
-    this.elements.linkMenu = document.querySelector('.header-global__content-item')
+    this.elements.containernav = document.querySelector('.header-global__nav-wrapper')
+    this.elements.linksmenu = document.querySelectorAll('.header-global__content-item')
   }
 
   setEvents () {
-    this.node.addEventListener('click', (this.eventsMenu.bind(this)))
-    this.elements.linkMenu.addEventListener('click', (this.eventsMenu.bind(this)))
+    this.node.addEventListener('click', (this.eventsMenu.bind(this))) 
+    this.elements.containernav.addEventListener('click', (this.eventsMenu.bind(this)))
   }
 
-  eventsMenu () {
-    console.log('ok')
-    this.elements.header.classList.toggle('btn-pressed')
-    this.elements.openedmenu.classList.toggle('opened-menu')
+  eventsMenu (event) {
+    const clickedElement = event.target
+    if(clickedElement.classList.contains('header-global__content-item')){
+      this.elements.linksmenu.forEach((element, index) => {
+        this.elements.linksmenu[index].classList.remove('header-global__content-item--selected')
+      })
+      const index = Array.from(this.elements.linksmenu).indexOf(clickedElement)
+      this.elements.linksmenu[index].classList.toggle('header-global__content-item--selected')
+    }
+    else if(clickedElement.classList.contains('header-global__btnmenu') || clickedElement.classList.contains('header-global__btnmenu-line')){
+      this.elements.header.classList.toggle('btn-pressed')
+      this.elements.openedmenu.classList.toggle('opened-menu')
+    }
+    
   }
 }
