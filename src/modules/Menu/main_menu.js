39ef9f33125127be9c005270a3135454c9c15cb4
Yeison <jeison.zk6@hotmail.com>
2018-02-15 09:57:11 -0500

    adding final components to the menu

--- src/modules/Menu/main_menu.js
+++ src/modules/Menu/main_menu.js
@@ -21,6 +21,7 @@ export class Mainmenu {
     this.headerglobal = document.querySelector('.header-global')
     this.navigation = document.querySelector('.header-global__nav-wrapper')
     this.links = document.querySelectorAll('.header-global__nav-item')
+    this.sublinks = document.querySelectorAll('.header-global-nav-dropdown__link')
     this.elements.openedmenu = document.querySelector('.container')
   }
 
@@ -28,14 +29,46 @@ export class Mainmenu {
     this.btnhamburger.addEventListener('click', () => {
       this.headerglobal.classList.toggle('btnhamburger-pressed')
       this.elements.openedmenu.classList.toggle('opened-menu')
+      this.links.forEach((element, index) => {
+        this.links[index].classList.remove('link-selected')
+      })
+      this.sublinks.forEach((element, index) => {
+        this.sublinks[index].classList.remove('sublink-selected')
+      })
     })
     this.navigation.addEventListener('click', (this.showsubmenu.bind(this)))
   }
 
   showsubmenu (event) {
     const clickedElement = event.target.closest('.header-global__nav-item')
-    if (clickedElement.classList.contains('header-global__nav-item')) {
-      clickedElement.classList.toggle('link-selected')
+    const clickedSubelement = event.target.closest('.header-global-nav-dropdown__link')
+    const indexlink = Array.from(this.links).indexOf(clickedElement)
+    const indexsublink = Array.from(this.sublinks).indexOf(clickedSubelement)
+    if (clickedElement !== null) {
+      if (clickedElement === this.links[indexlink]) {
+        this.links.forEach((element, index) => {
+          if (index !== indexlink) {
+            this.links[index].classList.remove('link-selected')
+          }
+        })
+        this.sublinks.forEach((element, index) => {
+          this.sublinks[index].classList.remove('sublink-selected')
+        })
+        if (clickedSubelement !== this.sublinks[indexsublink]) {
+          clickedElement.classList.toggle('link-selected')
+        } else if (clickedSubelement === this.sublinks[indexsublink]) {
+          this.sublinks.forEach((element, index) => {
+            if (index !== indexsublink) {
+              this.sublinks[index].classList.remove('sublink-selected')
+            }
+          })
+          clickedSubelement.classList.toggle('sublink-selected')
+        }
+      }
+    } else {
+      this.links.forEach((element, index) => {
+        this.links[index].classList.remove('link-selected')
+      })
     }
   }
 }
