6d2a08db3c125cdbe5a928991a8c34287f335e37
“Yeiosn <jeison.zk6@hotmail.com>
2018-02-22 14:06:28 -0500

    adding element for hero1

--- src/main_index.js
+++ src/main_index.js
@@ -1,6 +1,8 @@
 import '../node_modules/reset-css/_reset.scss'
 
 import { Mainmenu } from './modules/Menu/main_menu'
+import { ContentImage } from './modules/content-image/content_image'
 
 /* eslint-disable */
 const mainmenu = new Mainmenu(document.body)
+const contentImage = new ContentImage(document.querySelector('.content-image'))
